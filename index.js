8d302fd57329ad11dd80ab54e00886989825270d
Saki <sbasken@gmail.com>
2023-01-03 12:33:39 -0800

    commit tree efa3f17bc9ed31884a13800e6c448eee9f3de420

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(``)
+    
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(names, eventName) {
+    const messages = [];
+    let i = 0;
+    while(i < names.length) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+        i++;
+    }
+    return messages;
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown(num) {
+    for (let i=num; i > -1; i--) {
+        console.log(i);
+    }
+}
+
+countDown(10)
