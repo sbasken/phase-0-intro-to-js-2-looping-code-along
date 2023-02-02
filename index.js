728881f92d1577aa1b100f83dc3a3728efda0de1
Saki <sbasken@gmail.com>
2023-02-01 19:17:43 -0800

    add writeCards and countDown functions

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, occasion) {
+    let messages = [];
+    for (let name of names) {
+        messages.push(`Thank you, ${name}, for the wonderful ${occasion} gift!`)
+    }
+    return messages
+}
+
+function countDown(num) {
+    for (let i = num; num >= 0; num--) {
+        console.log(num)
+    }
+
+}
+
+countDown(9)
